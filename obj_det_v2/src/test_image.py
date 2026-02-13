8902082e52179999250352d2298d16d0d277a858
SuperDuu <vuducdu2k5@gmail.com>
2026-02-13 11:40:35 +0700

    add results

--- obj_det_v2/src/test_image.py
+++ obj_det_v2/src/test_image.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 YOLO_XML = str(BASE_DIR / "models" / "best_openvino_model" / "best.xml")
 CNN_XML = str(BASE_DIR / "models" / "openvino_cnn_v2" / "classifier_v2.xml")
 LABEL_JSON = str(BASE_DIR / "models" / "labels_v2.json")
-IMAGE_PATH = str(BASE_DIR / "datasets" / "user_test" / "img_test_user4.jpg")
+IMAGE_PATH = str(BASE_DIR / "datasets" / "user_test" / "img_test_user6.jpg")
 
 ie = Core()
 try:
@@ -62,8 +62,8 @@ for box in detected_boxes:
     is_target = "real" in label.lower() or "r1" in label.upper()
     color = (0, 255, 0) if (is_target and conf > 0.85) else (0, 0, 255)
     cv2.rectangle(display_frame, (x1, y1), (x2, y2), color, 2)
-    cv2.putText(display_frame, f"{label} {conf:.2f}", (x1, y1-10), 
-                cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
+    cv2.putText(display_frame, f"{label} {conf:.2f}", (x1-10, y1-5), 
+                cv2.FONT_HERSHEY_SIMPLEX, 0.35, color, 1)
 
     if is_target and conf > 0.85:
         area = (x2 - x1) * (y2 - y1)
@@ -80,8 +80,8 @@ if target_point:
     cv2.line(display_frame, (screen_center_x, h_orig), (px, py), (255, 255, 0), 2)
     cv2.putText(display_frame, f"ERR: {error_x}", (px + 15, py), 
                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
-
+display_resized = cv2.resize(display_frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
 print(f"Test kết thúc. Error gửi UART: {error_x if target_point else 0}")
-cv2.imshow("Test Robocon 2026 - Image Mode", display_frame)
+cv2.imshow("Test Robocon 2026 - Image Mode", display_resized)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
