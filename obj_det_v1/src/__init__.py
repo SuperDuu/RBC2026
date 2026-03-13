e94cd125338d37382b22eea0bca33f6d75cdcdeb
SuperDuu <vuducdu2k5@gmail.com>
2026-03-14 01:06:29 +0700

    final cleanup

--- obj_det_v1/src/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-RBC2026 Robocon Vision System Source Package.
-Author: Vu Duc Du
-"""
-
-__version__ = "2.3.0"
--- obj_det_v3/test.py
+++
@@ -1,7 +0,0 @@
-import timm
-import torch
-import torch.nn as nn
-
-# Kiểm tra model có sẵn
-model = timm.create_model('ghostnetv3_100', pretrained=True, features_only=True)
-print(model.feature_info.channels())  # [16, 24, 40, 80, 160] tùy variant
